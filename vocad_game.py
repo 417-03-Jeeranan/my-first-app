ed340588dcbec7e610cfeacf31b44756645f0f48
417-03-Jeeranan <54200@yupparaj.ac.th>
2026-09-11 09:00:39 +0700

    Update vocad_game.py

--- vocad_game.py
+++ vocad_game.py
@@ -109,8 +109,8 @@ ans4 = st.text_input(
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
 
-st.session_state.ans1_val = ans3
-st.session_state.ans2_val = ans4# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
 
 # 4. ปุ่มส่งคำตอบ
